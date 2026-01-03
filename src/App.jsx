652ed77c94c9654c860c5331f64755b6be7c3e79
Sanguine Feranmi <feranmibayeri@gmail.com>
2026-01-03 15:28:10 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/header"; 
-import Landing from "./components/Landing/landing";
+import Landing from "./components/landing/landing";
 import Properties from "./components/properties/properties";
 import Banner from "./components/banner";
 import About from "./components/about/About";
