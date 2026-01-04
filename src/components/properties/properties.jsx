2c7ad08976cf32d1038db6e312286d58c87bf524
Sanguine Feranmi <feranmibayeri@gmail.com>
2026-01-04 21:42:27 +0100

    fourth

--- src/components/properties/properties.jsx
+++ src/components/properties/properties.jsx
@@ -1,5 +1,10 @@
 export default function Properties (){
     return(
-        <div>Properties Component</div>
+        <div>
+            <div className="txt">
+                <h1>Properties Page</h1>
+                <p>Welcome to Estatein, where your dream property awaits in every corner of our beautiful world. Explore our curated selection of properties, each offering a unique story and a chance to redefine your life. With categories to suit every dreamer, your journey </p>
+            </div>
+        </div>
     )
 }
